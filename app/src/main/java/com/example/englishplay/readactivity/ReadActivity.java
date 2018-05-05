ecaabb594036e48f320fbc02941fad53a1789ab9
wangyufei <1650043869@qq.com>
2018-05-05 18:42:25 +0800

    调用相机

---
+++ app/src/main/java/com/example/englishplay/readactivity/ReadActivity.java
@@ -0,0 +1,52 @@
+package com.example.englishplay.readactivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.example.englishplay.R;
+
+public class ReadActivity extends AppCompatActivity {
+
+    Button capButton;
+    Button readButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_read);
+        init();
+
+    }
+
+    private void init() {
+        capButton = findViewById(R.id.CAP_Button);
+        readButton = findViewById(R.id.READ_Button);
+
+        capButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toCamera = new Intent(ReadActivity.this, CameraActivity.class);
+                startActivity(toCamera);
+            }
+        });
+    }
+//        readButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent toCamera = new Intent(ReadActivity.this, CameraActivity.class);
+//                startActivity(toCamera);
+//            }
+//        });
+}
