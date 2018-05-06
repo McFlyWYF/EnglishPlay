c3e7ed0ec36edfdc8de7497fb47a0ef92a7bcc97
wangyufei <1650043869@qq.com>
2018-05-06 17:18:07 +0800

    学单词

---
+++ app/src/main/java/com/example/englishplay/learnactivity/Words.java
@@ -0,0 +1,24 @@
+package com.example.englishplay.learnactivity;
+
+/**
+ * Created by 小飞侠 on 2018/5/6.
+ */
+
+public class Words {
+
+    private String name;
+    private int imageId;
+
+    public Words(String name,int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
---
+++ app/src/main/java/com/example/englishplay/learnactivity/WordsAdapter.java
@@ -0,0 +1,80 @@
+package com.example.englishplay.learnactivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.englishplay.R;
+
+import java.util.List;
+
+/**
+ * Created by 小飞侠 on 2018/5/6.
+ */
+
+public class WordsAdapter extends RecyclerView.Adapter<WordsAdapter.ViewHolder>{
+
+    private static final String TAG = "WordsAdapter";
+
+    private Context mContext;
+
+    private List<Words> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View view) {
+            super(view);
+            cardView = (CardView) view;
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public WordsAdapter(List<Words> fruitList) {
+        mFruitList = fruitList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mContext == null) {
+            mContext = parent.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.word_item, parent, false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Words fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext, WordsActivity.class);
+                intent.putExtra(WordsActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(WordsActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Words fruit = mFruitList.get(position);
+        holder.fruitName.setText(fruit.getName());
+        Glide.with(mContext).load(fruit.getImageId()).into(holder.fruitImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+}
